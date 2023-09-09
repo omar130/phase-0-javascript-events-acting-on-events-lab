5a66bb10ab56e58515f99cc00f3f68d5d3ff5d83
Omar Richards <omarrichards01@gmail.com>
2023-09-09 10:39:15 -0400

    lab

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Your code here
-const dodger = document.getElementById("dodger"); 
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if 
+  (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
 
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
@@ -14,7 +22,8 @@ function moveDodgerRight() {
   const rightNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(rightNumbers, 10);
 
-if (left > 360) {
-  dodger.style.left = `${left + 1}px`;
-}
+  
+  if (left > 40) {
+    dodger.style.left = `${left + 1}px`
+  }
 }
